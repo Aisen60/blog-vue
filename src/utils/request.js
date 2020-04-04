5bdd706411f4166fd486baa60706b7dae39298c9
Aisen60 <caixusheng60@gmail.com>
2020-04-04 14:53:38 +0800

    fix OAuth access token

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,40 +4,40 @@ import { MessageBox, Message } from "element-ui";
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  timeout: 60000 // request timeout
+  timeout: 60000, // request timeout
 });
 
 service.interceptors.request.use(
-  config => {
+  (config) => {
     config.headers.Authorization = defaultSettings.githubToken;
     return config;
   },
-  error => {
+  (error) => {
     console.log(error);
     return Promise.reject(error);
   }
 );
 
 service.interceptors.response.use(
-  response => {
+  (response) => {
     const res = response.data;
     if (response.status !== 200) {
       Message({
         message: res.message || "Error",
         type: "error",
-        duration: 5 * 1000
+        duration: 5 * 1000,
       });
       return Promise.reject(new Error(res.message || "Error"));
     } else {
       return res;
     }
   },
-  error => {
+  (error) => {
     console.log("err" + error); // for debug
     Message({
       message: error.message,
       type: "error",
-      duration: 5 * 1000
+      duration: 5 * 1000,
     });
     return Promise.reject(error);
   }
