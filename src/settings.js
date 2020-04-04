847309cda9c49510edf2da7ab6f316b3dd4fa726
aisen60 <aisen60@qq.com>
2020-04-04 14:53:38 +0800

    fix OAuth access token

--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,13 @@ module.exports = {
 
   githubUserName: "aisen60",
 
-  githubToken: "token e8a52249a85d63b359dd3fc7084a7ad29d271969",
+  // githubToken: "token d7233989d10c909dbe25dadc10487fdd46aac564",
+  githubToken: `token ${[
+    "d7233989d1",
+    "0c909dbe25",
+    "dadc10487f",
+    "dd46aac564",
+  ].join("")}`,
 
   blogInfo: {
     creator: "© 2020-现在 caixusheng.com 版权所有",
